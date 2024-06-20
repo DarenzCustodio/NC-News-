c919e75ba9274cc1217602114fe8a68bf3ec413e
Darenz Custodio <darenzcustodio@outlook.com>
2024-06-20 13:38:28 +0100

    add changes for articles page, article cards for each individual articles with view button

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,21 +4,26 @@ import '../../styling/ArticleCard.css'
 export default function ArticleCard({articles}){
 
     return (
-         <div>
+         <div className="flex-Container">
             {articles.article.map((article, index)=>{
                 return (
-                    <div className="articleContainer" 
-                    key={article.article_id}>
-                        <div className='articleTitle'>
-                            <h2>article {index + 1}</h2>
-                            <img className="articleImage"src={article.article_img_url} alt="image of article"></img> 
-                            <p>title: {article.title}</p>
-                            <p>{article.comment_count} comments</p>
-                        <div>              
-                            <Link className="viewComponent" to={`/articles/${article.article_id}`}>View</Link>
+                    <div >
+                        <div className='card'>
+                        <img className="articleImage" src={article.article_img_url} alt="image of article"></img> 
+                        <div className='card-content'>
+                            <p className='topic'>{article.topic}</p>
+                            <p className='heading'> {article.title}</p>
+                            <p className='author'>by {article.author}</p>
+                            <p>{article.comment_count} comments</p>           
+                            <Link className="btn" to={`/articles/${article.article_id}`}>View</Link>
+                        </div>
+                    <div>
+                        </div>
+                        <div>
                         </div>
                         </div>
-                    </div>
+                        </div>
+                        
                 )
             })}
         </div>
